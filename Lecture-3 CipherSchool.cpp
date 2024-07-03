6e4fb5284ddeb84ffc3d270656a8bc0b153a559a
adityrajtiwary <122718170+adityrajtiwary@users.noreply.github.com>
2024-07-03 11:48:01 +0530

    Add files via upload

---
+++ Lecture-3 CipherSchool.cpp
@@ -0,0 +1,15 @@
+// In this lecture 3 we have learn about what is variable and constant in c++ 
+// Variable is basically a container where we can store any data which we want as per our requirement
+//This is why we are having the differerent data types present in c++ which helps the compiler to understand whatever the variable we are making those variable can only stored the data matching with their data type requirement
+//we genereally learn about: INT, FLOAT, CHAR, BOOL
+//In INT(integer):= we can only stored numbers , not decimal but only fixed numbers
+//In FLOAT := This is same as INT but in this we can stored the decimal as well for example : 1.5, 2.8
+//In CHAR := This data type are used to store the single letter character for example: 'A', 'B'
+//In Bool(BOOLEAN) := used to stored the value as True or False for example: 0 = false, 1 = true
+
+// Then we learned about what is constant , so constant is the value which is going to be fixed for everyone example:= 3
+
+// Then we have also learn about how to initialize the variable in c++ for example = The variable must begin with (A-Z or a-z) or it can be also with the underscore 
+//after the first character we can add number also in it for example := S1_sahil, _sahil
+//One thing which we need to notice whenver we are creating any variable those are need to be unique from the other because if no they will going to generate the error 
+//Note: int a, A;  these both two variable with the same name will act different in c++ because both the value is having different ascii value so compiler is litreally going to treat themm as the different 
